54948454df09058f9c2b7a474ca088e266812375
ignacio mendez <ignacio.mendez0000@gmail.com>
2021-04-09 19:45:58 -0300

    init

---
+++ src/test/java/unq/edu/ar/grupoMs12021/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package unq.edu.ar.grupoMs12021.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
