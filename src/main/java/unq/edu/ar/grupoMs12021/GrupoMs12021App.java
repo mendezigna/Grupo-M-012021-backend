9e9be8a102c0de47e2b0962f0b3179b432492ac2
ignacio mendez <ignacio.mendez0000@gmail.com>
2021-04-10 19:29:31 -0300

    project structure

---
+++ src/main/java/unq/edu/ar/grupoMs12021/GrupoMs12021App.java
@@ -0,0 +1,13 @@
+package unq.edu.ar.grupoMs12021;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrupoMs12021App {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrupoMs12021App.class, args);
+	}
+
+}
