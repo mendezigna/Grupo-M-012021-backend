0d2e25fbeedb0ddec16b3114e9555e9c00fe62c0
ignacio mendez <ignacio.mendez0000@gmail.com>
2021-04-10 19:29:11 -0300

    project structure

--- src/main/java/unq/edu/ar/grupoMs12021/ServletInitializer.java
+++ src/main/java/unq/edu/ar/grupoMs12021/ServletInitializer.java
@@ -1,13 +1,14 @@
-package unq.edu.ar.grupoMs12021.demo;
+package unq.edu.ar.grupoMs12021;
 
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import unq.edu.ar.grupoMs12021.GrupoMs12021App;
 
 public class ServletInitializer extends SpringBootServletInitializer {
 
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(DemoApplication.class);
+		return application.sources(GrupoMs12021App.class);
 	}
 
 }
--- src/test/java/unq/edu/ar/grupoMs12021/GrupoMs12021AppTests.java
+++ src/test/java/unq/edu/ar/grupoMs12021/GrupoMs12021AppTests.java
@@ -1,10 +1,10 @@
-package unq.edu.ar.grupoMs12021.demo;
+package unq.edu.ar.grupoMs12021;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class GrupoMs12021AppTests {
 
 	@Test
 	void contextLoads() {
