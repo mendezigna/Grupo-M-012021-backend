e690a339a1ac37eaae9f4301fa4c5b86c656c674
ignacio mendez <ignacio.mendez0000@gmail.com>
2021-04-15 17:07:36 -0300

    fix errors

--- src/test/kotlin/unq/edu/ar/GrupoMs12021/Resenia/webservice/ReseniaApplicationTests.kt
+++ src/test/kotlin/unq/edu/ar/GrupoMs12021/Resenia/webservice/ReseniaApplicationTests.kt
@@ -1,9 +1,9 @@
-package unq.edu.ar.GrupoMs12021.Resenia
+package unq.edu.ar.GrupoMs12021.Resenia.webservice
 
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 
-@SpringBootTest
+@SpringBootTest()
 class ReseniaApplicationTests {
 
 	@Test
