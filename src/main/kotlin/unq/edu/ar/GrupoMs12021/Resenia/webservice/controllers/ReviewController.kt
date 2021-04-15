72f75ff97a4e1f2806134a202d6a600188a5dbb8
ignacio mendez <ignacio.mendez0000@gmail.com>
2021-04-15 16:59:35 -0300

    controllers

--- src/main/kotlin/unq/edu/ar/GrupoMs12021/Resenia/webservice/controllers/ReviewController.kt
+++ src/main/kotlin/unq/edu/ar/GrupoMs12021/Resenia/webservice/controllers/ReviewController.kt
@@ -1,9 +1,11 @@
 package unq.edu.ar.GrupoMs12021.Resenia.webservice.controllers
 
 import org.springframework.web.bind.annotation.CrossOrigin
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @CrossOrigin(origins = ["*"])
+@RequestMapping("/review")
 class ReviewController {
 }
