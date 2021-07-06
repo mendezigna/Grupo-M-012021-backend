c634ee30a954406c6f24aad852f61f983a425900
vqzmariana <mariaanavqz@gmail.com>
2021-07-06 10:35:40 -0300

    fix controller

--- src/main/kotlin/unq/edu/ar/GrupoMs12021/Resenia/webservice/controllers/CacheController.kt
+++ src/main/kotlin/unq/edu/ar/GrupoMs12021/Resenia/webservice/controllers/CacheController.kt
@@ -1,19 +1,21 @@
 package unq.edu.ar.GrupoMs12021.Resenia.webservice.controllers
 
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.web.bind.annotation.CrossOrigin
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 import unq.edu.ar.GrupoMs12021.Resenia.service.CacheService
+import unq.edu.ar.GrupoMs12021.Resenia.webservice.aspects.ApiKey
+import unq.edu.ar.GrupoMs12021.Resenia.webservice.aspects.Log
+import unq.edu.ar.GrupoMs12021.Resenia.webservice.dto.TitleLite
 
 @RestController
 @CrossOrigin(origins = ["*"])
 @RequestMapping("/cache")
 class CacheController(@Autowired private val cacheService: CacheService) {
 
-    @GetMapping
-    fun getByTitle(){
-        this.cacheService.getByTitle("sadfadsf")
+    @Log
+    @ApiKey
+    @GetMapping("{titleId}")
+    fun getByTitle(@PathVariable titleId: String): TitleLite? {
+        return this.cacheService.getByTitle(titleId)
     }
 }
